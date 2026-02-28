048966c850454599e987cb6e5ebc11ddc6dabd50
AichaLheimeur <aicha.lheimeur2003@gmail.com>
2026-02-28 22:13:13 +0100

    feat(db): add books table

--- db/init/001_schema.sql
+++ db/init/001_schema.sql
@@ -12,3 +12,16 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- =========================
+-- TABLE BOOKS
+-- =========================
+CREATE TABLE IF NOT EXISTS books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    category VARCHAR(100),
+    description TEXT,
+    total_quantity INT DEFAULT 1,
+    available_quantity INT DEFAULT 1,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
