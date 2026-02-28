4687eb13819c8892634d03c6935c4a155c30ce64
AichaLheimeur <aicha.lheimeur2003@gmail.com>
2026-02-28 22:11:07 +0100

    feat(db): add users table

--- db/init/001_schema.sql
+++ db/init/001_schema.sql
@@ -1,2 +1,14 @@
 CREATE DATABASE IF NOT EXISTS library_db;
 USE library_db;
+-- =========================
+-- TABLE USERS
+-- =========================
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('USER', 'ADMIN') DEFAULT 'USER',
+    is_validated BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
