37d99c6221e9ed9677c588941af3e38047151edb
AichaLheimeur <aicha.lheimeur2003@gmail.com>
2026-02-28 21:06:02 +0100

    feat: connect backend to MySQL and add health DB route

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const healthRoutes = require("./src/routes/health");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.json({ message: "Backend is running 🚀" });
+app.use(express.json());
+
+// Brancher les routes
+app.use("/health", healthRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Backend is running 🚀" });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
